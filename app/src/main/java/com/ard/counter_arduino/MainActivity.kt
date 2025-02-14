0adca69e84d46bc36c8911a62a4774b43e93a63b
j43hyun9 <j43hyun9@gmail.com>
2025-02-14 23:19:55 +0900

    Fragment 를 활용하여 백 스택을 사용하면 앱 간 화면 이동이 자연스럽다.

--- app/src/main/java/com/ard/counter_arduino/MainActivity.kt
+++ app/src/main/java/com/ard/counter_arduino/MainActivity.kt
@@ -1,45 +1,23 @@
 package com.ard.counter_arduino
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
+import android.view.LayoutInflater
 import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ard.counter_arduino.ui.theme.Counter_ArduinoTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val add_Product: Button = findViewById(R.id.add_product)
-        val list_Product: Button = findViewById(R.id.list_product)
-        val delete_Product : Button = findViewById(R.id.delete_product)
-        val pre_Button : Button = findViewById(R.id.pre_Button)
-
-        add_Product.setOnClickListener {
-            add_Product.visibility = View.GONE
-            list_Product.visibility = View.GONE
-            delete_Product.visibility = View.GONE
-            pre_Button.visibility = View.VISIBLE
-        }
-
-        pre_Button.setOnClickListener( {
-            add_Product.visibility = View.VISIBLE
-            list_Product.visibility = View.VISIBLE
-            delete_Product.visibility = View.VISIBLE
-            pre_Button.visibility = View.GONE
-        })
+        container = findViewById(R.id.fragment_container)
 //        setContent {
 //            Counter_ArduinoTheme {
 //                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -51,21 +29,5 @@ class MainActivity : ComponentActivity() {
 //            }
 //        }
 
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Counter_ArduinoTheme {
-        Greeting("Android")
     }
 }
