7c8d9e2343570cc3db98c9e1b9be6378b779bc42
j43hyun9 <j43hyun9@gmail.com>
2025-02-15 11:52:40 +0900

    여기서부터 처음부터 시작

---
+++ app/src/main/java/com/ard/counter_arduino/FirstFragment.kt
@@ -0,0 +1,28 @@
+package com.ard.counter_arduino
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        val button = view.findViewById<Button>(R.id.button)
+        button.setOnClickListener() {
+            val fragment = SecondFragment()
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.fragment_container, fragment)
+            transaction?.addToBackStack(null)
+            transaction?.commit()
+        }
+
+        return view
+    }
+}
--- app/src/main/java/com/ard/counter_arduino/MainActivity.kt
+++ app/src/main/java/com/ard/counter_arduino/MainActivity.kt
@@ -1,33 +1,19 @@
 package com.ard.counter_arduino
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.Button
-import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ard.counter_arduino.ui.theme.Counter_ArduinoTheme
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        container = findViewById(R.id.fragment_container)
-//        setContent {
-//            Counter_ArduinoTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//            }
-//        }
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FirstFragment())
+                .commit()
+        }
 
     }
 }
